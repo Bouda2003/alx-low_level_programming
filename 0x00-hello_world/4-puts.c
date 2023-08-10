ca7d2625c412640fa3d8802d1df8c3b05d9999c9
Abd-Elrahman <aute.mohsen@gmail.com>
2023-08-10 10:49:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-int puts(const char* Programming is like building a multilingual puzzle);
+#include <stdio.h>
+int main(void){
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

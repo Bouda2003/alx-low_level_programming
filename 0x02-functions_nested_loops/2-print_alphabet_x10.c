22b02a4698ebcd44c1be86cfa9575e6bfa1b896d
Abd-Elrahman <aute.mohsen@gmail.com>
2023-08-15 13:20:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Describtion: Write a function that prints 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int r;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		-putchar('\n');
+	}
+}

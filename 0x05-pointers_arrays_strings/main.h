6686dfc7d7644c211d3ffc79c7b9820b5b76b04b
Abd-Elrahman <aute.mohsen@gmail.com>
2023-08-21 10:18:16 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 void reset_to_98(int *n);
 
@@ -23,7 +25,4 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
-
-
-#endif
-
+#endif /* MAIN_H */

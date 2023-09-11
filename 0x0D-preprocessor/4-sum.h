8666dc0b88aca28204769282e65431047396d07c
Abd-Elrahman <aute.mohsen@gmail.com>
2023-09-11 09:29:38 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,11 +3,11 @@
 
 /*
  * File: 4-sum.h
- * Auth: Abdelrahman_Mohsen
- * Desc: Header file that defines a function-like macro SUM(x, y) 
- * 	that computes the sum of the numbers x and y.
+ * Auth: Abdelrahman Mohsen
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
 
-#define SUM(x,y) ((x) + (y))
+#define SUM(x, y) ((x) + (y))
 
 #endif

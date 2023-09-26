69d2627651d20dfeda6f8c829e5ba52ad517d4f2
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2023-09-26 12:30:32 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nint_at_index - Locates a given n of
+ * get_nodeint_at_index - Locates a given n of
  *                        a listint_t linked list.
  * @head: A pointer to the head of the listint_t list.
  * @index: The index of the n to locate - indices start at 0.

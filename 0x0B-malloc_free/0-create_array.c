e878591ad8d5becaf47c1b66ccaf8ba94c1a0f5c
root <aute.mohsen@gmail.com>
2024-01-15 12:47:46 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *array;
 array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-for(i = 0 ; i < size ; i++)
+for (i = 0 ; i < size ; i++)
 	array[i] = c;
 		return (array);
 

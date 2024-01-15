44922055c7360e36b11b10a761539d737d3ab372
root <aute.mohsen@gmail.com>
2024-01-15 13:06:59 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,23 +9,24 @@
  */
 char *_strdup(char *str)
 {
-char *copy;
-int i, len = 0;
+	char *copy;
+	int i, len = 0;
 
-if (str == NULL)
-	return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-for (i = 0; i < str[i]; i++)
-	len++;
+	for (i = 0; str[i]; i++)
+		len++;
 
-copy = malloc(sizeof(char) * (len + 1));
+	copy = malloc(sizeof(char) * (len + 1));
 
-if (copy == NULL)
-	return (NULL);
+	if (copy == NULL)
+		return (NULL);
 
-for (i = 0 ; str[i]; i++)
-	copy[i] = str[i];
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
 
-copy[len] = '\0';
+	copy[len] = '\0';
+
 	return (copy);
 }

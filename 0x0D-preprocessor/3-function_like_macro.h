780dfb81077ca2ba7866a33f4b29349b7f3fa211
root <aute.mohsen@gmail.com>
2024-01-22 09:02:35 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Abdelrahman_Mohsen
+ * Desc: Header file that defines a macro named function_like_macro.h
+ */
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#endif
